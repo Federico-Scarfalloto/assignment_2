5ec3348deef3137ac4639c2ffcc163ac6ebff827
Federico Scarfalloto <scarf@DESKTOP-0MHT6OF>
2021-11-03 16:19:29 +0100

    Modified class, now with fields and methods

--- 01/src/main/java/com/scarfal/git/votapp/Amministratore.java
+++ 01/src/main/java/com/scarfal/git/votapp/Amministratore.java
@@ -1,5 +1,53 @@
 package com.scarfal.git.votapp;
 
 public class Amministratore {
+	//FIELDS
+	private String mail, username, psw;
+	private String name, surname;
+	private int bday, bmonth, byear;
 	
+	//CONSTRUCTORS
+	public Amministratore() {
+		mail="mail@mail.com";
+		username="username";
+		psw="password";
+		name="guest";
+		surname="guest";
+		bday= 1;
+		bmonth=1;
+		byear=1900;
+	}
+	
+	public Amministratore(String mail,String username,String psw,String name,String surname,int bday,int bmonth,int byear) {
+		this.mail = mail;
+		this.username=username;
+		this.psw=psw;
+		this.name=name;
+		this.surname=surname;
+		this.bday=bday;
+		this.bmonth=bmonth;
+		this.byear=byear; 
+	}
+	
+	//METHODS
+	public String getUsername() {
+		return "";
+	}
+	
+	public String getPassword() {
+		return "";
+	}
+	
+	public boolean changeUsername() {
+		return false;
+	}
+	
+	public boolean changePassword() {
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		return "";
+	}
 }
--- 01/src/main/java/com/scarfal/git/votapp/Elettore.java
+++ 01/src/main/java/com/scarfal/git/votapp/Elettore.java
@@ -1,5 +1,38 @@
 package com.scarfal.git.votapp;
 
 public class Elettore {
-
+	//FIELDS
+	private String id, name, surname, num_tes_elet; 
+	private int bday, bmonth, byear;
+	
+	//CONSTRUCTORS
+	public Elettore(){
+		id = "00000000";
+		name = "guest";
+		surname = "guest";
+		bday = 1;
+		bmonth = 1;
+		byear = 1900;
+		num_tes_elet = "0000000000";
+	}
+	
+	public Elettore(String id, String name, String surname, int bday, int bmonth, int byear, String num_tes_elet){
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.bday = bday;
+		this.bmonth = bmonth;
+		this.byear = byear;
+		this.num_tes_elet = num_tes_elet;
+	}
+	
+	//METHODS
+	public boolean vote() {
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		return "";
+	}
 }
